61532c792694821ac6b886c7f9645854a6c521c6
Tadeu Juliace <tadeslopes@gmail.com>
2021-06-18 05:07:40 -0300

    post e get de rentals iniciado, porem ainda sem validaçoes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import pg from "pg";
 import express from "express";
 import Joi from "joi";
+import dayjs from "dayjs"
 pg.types.setTypeParser(1082, (str) => str);
 const { Pool } = pg;
 
@@ -135,10 +136,11 @@ app.post("/customers",async(req,res)=>{
         name: Joi.string().required(),
         phone: Joi.string().pattern(/^[0-9]{10,11}$/),
         cpf: Joi.string().pattern(/^[0-9]{11}$/),
+        birthday:Joi.date(),
     });
-    const {error}=schema.validate({name,phone,cpf});
+    const {error}=schema.validate({name,phone,cpf,birthday});
     if(error){
-        res.send(error);return
+        res.sendStatus(400);return
     }
 
     const cpfExist= await connection.query(`SELECT * FROM customers 
@@ -161,18 +163,18 @@ app.put("/customers/:id", async(req,res)=>{
         name: Joi.string().required(),
         phone: Joi.string().pattern(/^[0-9]{10,11}$/),
         cpf: Joi.string().pattern(/^[0-9]{11}$/),
+        birthday:Joi.date(),
     });
-    const {error}=schema.validate({name,phone,cpf});
+    const {error}=schema.validate({name,phone,cpf,birthday});
     if(error){
-        res.send(error);return
-    }
-
-    const cpfExist= await connection.query(`SELECT * FROM customers 
-    WHERE cpf=$1`,[cpf])
-    if(cpfExist.rows.length>0 && cpfExist.rows[0].id!=id){
-        res.sendStatus(409); return
+        res.sendStatus(400);return
     }
     try{
+        const cpfExist= await connection.query(`SELECT * FROM customers 
+        WHERE cpf=$1`,[cpf])
+        if(cpfExist.rows.length>0 && cpfExist.rows[0].id!=id){
+            res.sendStatus(409); return
+        }
         const changes= await connection.query(`UPDATE customers 
         SET name=$1,phone=$2,cpf=$3,birthday=$4
         WHERE id=$5`,[name,phone,cpf,birthday,id]);
@@ -181,6 +183,33 @@ app.put("/customers/:id", async(req,res)=>{
         res.sendStatus(err);
     }
 })
+// All rentals, registering, updating and deleting
+app.get("/rentals",async(req,res)=>{
+    try{
+        const rentals=await connection.query(`SELECT * FROM rentals`)
+        res.send(rentals.rows);
+    }catch{
+        res.sendStatus(400);
+    }
+})
+
+app.post("/rentals",async(req,res)=>{
+    const {customerId,gameId,daysRented}=req.body;
+    const rentDate=dayjs().format('YYYY-MM-DD');
+    try{
+        const gameInfo= await connection.query(`SELECT "pricePerDay" from games where id=$1`,[gameId])
+        const originalPrice=daysRented*gameInfo.rows[0].pricePerDay
+        const newRental= await connection.query(`INSERT INTO rentals 
+        ("customerId","gameId","rentDate","daysRented","returnDate","originalPrice","delayFee")
+        VALUES ($1,$2,$3,$4,$5,$6,$7)`,
+        [customerId,gameId,rentDate,daysRented,null,originalPrice,null])
+        res.sendStatus(200)
+    }catch(err){
+        res.send(err)
+    }
+});
+
+
 
 app.listen(4000, () => {
     console.log('Server listening on port 4000.');
